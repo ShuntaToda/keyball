b0df45ac36d7d9b43ba7bfc61194f367e5024df1
ShuntaToda <todashuntawork@gmail.com>
2024-12-23 14:39:00 +0900

    Automatic Mouse Layer機能を追加

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/my_setting/config.h
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/my_setting/config.h
@@ -39,3 +39,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define AUTO_MOUSE_DEFAULT_LAYER 1
 
 #define DYNAMIC_KEYMAP_LAYER_COUNT 5
+
+#define AUTO_MOUSE_DEFAULT_LAYER 4 // 切り替えるマウスレイヤー番号を指定
+#define AUTO_MOUSE_TIME 500 // マウスが止まってから元のレイヤーに戻るまでの時間(ms)
